a12ab510e21e1d4c13daf94c6f5ddcad32ecf9d3
Chris Dudley <chris.dudley@meetcleo.com>
2023-12-08 13:00:38 +0000

    Use quadratic

--- day_6/day_6_solution.rb
+++ day_6/day_6_solution.rb
@@ -1,28 +1,31 @@
+require 'byebug'
+
 class Day6
 
-  def part_1(test: false)
+  def initialize(test: false)
     File.readlines("day_6/day_6_#{'test_' if test}input.txt", chomp: true).each do |line|
       @times = line.split(' ')[1..-1].map(&:to_i) if line.include?('Time')
       @distances = line.split(' ')[1..-1].map(&:to_i) if line.include?('Distance')
     end
-
-    @times.zip(@distances).map { |t, d| (1..t).select { |ms| (t - ms) * ms > d }.count }.inject(:*)
   end
 
-  def part_2(test: false)
-    File.readlines("day_6/day_6_#{'test_' if test}input.txt", chomp: true).each do |line|
-      @time = line.split(' ')[1..-1].join.to_i if line.include?('Time')
-      @distance = line.split(' ')[1..-1].join.to_i if line.include?('Distance')
-    end
+  def part_1
+    @times.zip(@distances).map { |t, d| quadratic_range(t, d) }.inject(:*)
+  end
 
-    (1..@time).select { |ms| (@time - ms) *     ms > @distance }.count
+  def part_2
+    quadratic_range(@times.join.to_i, @distances.join.to_i)
+  end
+
+  def quadratic_range(b, c)
+    ((-b - Math.sqrt(b**2 - 4 * c)) / (-2)).ceil.pred - ((-b + Math.sqrt(b**2 - 4 * c)) / (-2)).floor.next + 1
   end
 end
 
 puts "PART 1"
-puts "\tTEST INPUT RESULT = #{Day6.new.part_1(test: true)}"
+puts "\tTEST INPUT RESULT = #{Day6.new(test: true).part_1}"
 puts "\tACTUAL INPUT RESULT = #{Day6.new.part_1}"
 
 puts "\nPART 2"
-puts "\tTEST INPUT RESULT = #{Day6.new.part_2(test: true)}"
+puts "\tTEST INPUT RESULT = #{Day6.new(test: true).part_2}"
 puts "\tACTUAL INPUT RESULT = #{Day6.new.part_2}"
